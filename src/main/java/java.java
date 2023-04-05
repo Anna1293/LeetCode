933b9713631c1ca320cbe8d1ba5d72ae66d50493
olegkudryaev <oleg.kudraev@gmail.com>
2023-04-05 11:24:00 +0300

    123

---
+++ src/main/java/java.java
@@ -0,0 +1,11 @@
+public class java {
+
+    public static void main(String[] args) {
+        System.out.println(sum(4, 5));
+    }
+
+    public static int sum(int num1, int num2) {
+        return (num1+num2);
+    }
+
+}
